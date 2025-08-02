e891e4de1021fd230f7783c3d899ffa1a54984a1
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-08-03 03:29:46 +0530

    Initial Page Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
-
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <div>
+        <BrowserRouter>
+        <Navbar/>
+        
+        </BrowserRouter>
+        
+      </div>
       
     </>
   )
